2a486442f9084fae0e4d715e8eb18d6175f83ee5
highalps <highalps93@gmail.com>
2019-12-02 01:05:07 +0900

    chore: Initail project setup

---
+++ Dotnet.Sandbox.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dotnet.Sandbox.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
